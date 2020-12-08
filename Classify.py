01915568828d247526c572da79219f5b6564b415
mjkim2010 <46270237+mjkim2010@users.noreply.github.com>
2020-12-08 15:11:27 -0800

    Update Classify.py

--- Classify.py
+++ Classify.py
@@ -4,27 +4,21 @@ Acknowledgements to @author: vsnick
 """
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-#Import training da
 train = pd.read_csv('train2.csv')
 train.symptoms
 train.disease
-#Vectorizing the symptoms
+# Vectorizing the symptoms
 tfid_vectorizer = TfidfVectorizer(min_df=1)
 X = tfid_vectorizer.fit_transform(train.symptoms)
-#print X
 tfid_vectorizer.get_feature_names()
-#print tfid_vectorizer.get_feature_names()
 
-#Vectorizing the diseases
+# Vectorizing the diseases
 from sklearn import preprocessing
 le = preprocessing.LabelEncoder()
 Y = le.fit_transform(train.disease)
 le.classes_
-#print Y
-#print le.inverse_transform(Y)
-#Fitting the model
 
-#implement Multinomial NB
+# implement Multinomial NB
 from sklearn.naive_bayes import MultinomialNB
 clf = MultinomialNB().fit(X,Y)
 
@@ -34,14 +28,14 @@ test.symptoms
 predicted = clf.predict_proba(tfid_vectorizer.transform(test.symptoms))
 prob = predicted.tolist()[0]
 prob = [x*100 for x in prob]
-#Probabilities of diseases
+# Probabilities of diseases
 output = dict(zip(le.classes_,prob))
 print pd.DataFrame(output.items())
-#Most probable
+# Most probable
 predicted = clf.predict(tfid_vectorizer.transform(test.symptoms))
 print le.classes_[predicted]
 
-#implement Binomial NB
+# implement Binomial NB
 from sklearn.naive_bayes import BernoulliNB
 clf = BernoulliNB().fit(X,Y)
 
@@ -51,10 +45,10 @@ test.symptoms
 predicted = clf.predict_proba(tfid_vectorizer.transform(test.symptoms))
 prob = predicted.tolist()[0]
 prob = [x*100 for x in prob]
-#Probabilities of diseases
+# Probabilities of diseases
 output = dict(zip(le.classes_,prob))
 print pd.DataFrame(output.items())
-#Most probable
+# Most probable
 predicted = clf.predict(tfid_vectorizer.transform(test.symptoms))
 print le.classes_[predicted]
 
